5710142e018a9ed24b39c8cc5cfd832c9c688a7c
richardojo04@gmail.com <richardojo04@gmail.com>
2018-07-05 00:49:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  elsif
+  array.length <= 3
+   puts "The line is currently: 1. #{array[0]} 2. #{array[1]} 3. #{array[2]}"
+  
+   else
+     puts "The line is currently: 1. #{array[0]} 2. #{array[1]} 3. #{array[2]} 4. #{array[3]} 5. #{array[4]} 6. #{array[5]} 7. #{array[6]} 8. #{array[7]} 9. #{array[8]} 10. #{array[9]}"
+ end
+end 
+
+def take_a_number(array, name)
+  array.push(name)
+ array.each{|x, index| puts "Welcome, #{name}. You are number #{index += 1} in line."}
+end
